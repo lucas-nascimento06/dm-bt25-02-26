21c7e4981b99a12b5ab96906808a099f4f5cbe01
Lucas Nascimento <laravelcrmsysttem@gmail.com>
2026-02-25 21:02:08 -0300

    nova alteracao

--- bot/codigos/handlers/message/confissoesHandler.js
+++ bot/codigos/handlers/message/confissoesHandler.js
@@ -5,7 +5,7 @@ class ConfissoesHandler {
 
     constructor() {
         // ⚙️ ID do grupo onde as confissões serão postadas
-        this.groupId = '';
+        this.groupId = '120363419322682521@g.us';
 
         // 🚫 LISTA DE PALAVRAS BLOQUEADAS
         this.palavrasBloqueadas = [
